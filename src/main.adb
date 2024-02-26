70ddb8dcb9a210591e284c60a205c472607abc1d
kapelyna <katiakaplya@gmail.com>
2024-02-26 21:33:26 +0200

    Lr_1/Ada

--- src/main.adb
+++ src/main.adb
@@ -3,10 +3,11 @@ with Ada.Text_IO;
 procedure Main is
 
    Can_Stop : Boolean := False;
-   Step : Long_Long_Integer := 2;
+   pragma Atomic (Can_Stop);
+   Step : Long_Long_Integer := 1;
 
    task type Stop_Thread;
-   task type Sum_Thread;
+   task type Sum_Thread(ID:Integer);
 
    task body Stop_Thread is
    begin
@@ -15,26 +16,31 @@ procedure Main is
    end Stop_Thread;
 
    task body Sum_Thread is
-      Terms : Long_Long_Integer := 0;
+      Additions : Long_Long_Integer := 0;
       Sum : Long_Long_Integer := 0;
    begin
       loop
          exit when Can_Stop;
-         Sum := Sum + (Terms * Step);
-         Terms := Terms + 1;
+         Sum := Sum + (Additions * Step);
+         Additions := Additions + 1;
       end loop;
 
 
-         Ada.Text_IO.Put_Line("Thread " & " - Sum: "&Sum'Img&" - Terms: "&Terms'Img);
+         Ada.Text_IO.Put_Line("Thread " &ID'Img& " - Sum: "&Sum'Img&" - Additions: "&Additions'Img);
 
 
    end Sum_Thread;
 
    B1 : Stop_Thread;
-   T1 : Sum_Thread;
-   T2 : Sum_Thread;
-   T3 : Sum_Thread;
-   T4 : Sum_Thread;
+
+   T1 : Sum_Thread(1);
+   T2 : Sum_Thread(2);
+   T3 : Sum_Thread(3);
+   T4 : Sum_Thread(4);
+   T5 : Sum_Thread(5);
+   T6 : Sum_Thread(6);
+
+
 
 begin
    null;
